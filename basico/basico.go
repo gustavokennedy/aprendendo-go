a0b09f38c6f6f37e45a40dd37e0fbd5ad7f7307c
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-16 16:25:30 -0300

    Map com make()

--- basico/basico.go
+++ basico/basico.go
@@ -147,7 +147,7 @@ func main() {
 
 /*
 	Array
-*/
+
 package main
 
 import (
@@ -160,4 +160,30 @@ func main() {
 
 	fmt.Println(arr1)
 	fmt.Println(arr2)
+}
+*/
+
+/*
+	Map com Make()
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = make(map[string]string) // The map is empty now
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+	// a is no longer empty
+	b := make(map[string]int)
+	b["Oslo"] = 1
+	b["Bergen"] = 2
+	b["Trondheim"] = 3
+	b["Stavanger"] = 4
+
+	fmt.Printf("a\t%v\n", a)
+	fmt.Printf("b\t%v\n", b)
 }
