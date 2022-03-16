4fcf490fd53d5efb9317f99ea58a0a219f5521d0
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-16 16:37:17 -0300

    Passar Estrutura como Argumentos de Função

--- basico/basico.go
+++ basico/basico.go
@@ -210,7 +210,7 @@ func main() {
 
 /*
 	Acessar membros da estrutura
-*/
+
 package main
 
 import (
@@ -251,4 +251,51 @@ func main() {
 	fmt.Println("Age: ", pers2.age)
 	fmt.Println("Job: ", pers2.job)
 	fmt.Println("Salary: ", pers2.salary)
+}
+*/
+
+/*
+	Passar Estrutura como Argumentos de Função
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
+}
+
+func main() {
+	var pers1 Person
+	var pers2 Person
+
+	// Pers1 specification
+	pers1.name = "Hege"
+	pers1.age = 45
+	pers1.job = "Teacher"
+	pers1.salary = 6000
+
+	// Pers2 specification
+	pers2.name = "Cecilie"
+	pers2.age = 24
+	pers2.job = "Marketing"
+	pers2.salary = 4500
+
+	// Print Pers1 info by calling a function
+	printPerson(pers1)
+
+	// Print Pers2 info by calling a function
+	printPerson(pers2)
+}
+
+func printPerson(pers Person) {
+	fmt.Println("Name: ", pers.name)
+	fmt.Println("Age: ", pers.age)
+	fmt.Println("Job: ", pers.job)
+	fmt.Println("Salary: ", pers.salary)
 }
