31df32b74c511a935e6628fbf205a2cb010347e8
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-16 16:23:23 -0300

    Array

--- basico/basico.go
+++ basico/basico.go
@@ -129,7 +129,7 @@ func threeTimes(msg string) (tMsg string) {
 
 /*
  Chamar funções
-*/
+
 package main
 
 import (
@@ -142,4 +142,22 @@ func minhaMensagem() {
 
 func main() {
 	minhaMensagem() // chama a func
+}
+*/
+
+/*
+	Array
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr1 = [3]int{1, 2, 3}
+	arr2 := [5]int{4, 5, 6, 7, 8}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
 }
