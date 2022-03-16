c6c949704602c5acf044295a5e031cd35c84734e
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-16 16:35:50 -0300

    Acessar membros da estrutura

--- basico/basico.go
+++ basico/basico.go
@@ -191,7 +191,7 @@ func main() {
 
 /*
 	Acessando elementos do map
-*/
+
 package main
 
 import (
@@ -205,4 +205,50 @@ func main() {
 	a["year"] = "1964"
 
 	fmt.Printf(a["brand"])
+}
+*/
+
+/*
+	Acessar membros da estrutura
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
+}
+
+func main() {
+	var pers1 Person
+	var pers2 Person
+
+	// Pers1 specification
+	pers1.name = "Hege"
+	pers1.age = 45
+	pers1.job = "Teacher"
+	pers1.salary = 6000
+
+	// Pers2 specification
+	pers2.name = "Cecilie"
+	pers2.age = 24
+	pers2.job = "Marketing"
+	pers2.salary = 4500
+
+	// Access and print Pers1 info
+	fmt.Println("Name: ", pers1.name)
+	fmt.Println("Age: ", pers1.age)
+	fmt.Println("Job: ", pers1.job)
+	fmt.Println("Salary: ", pers1.salary)
+
+	// Access and print Pers2 info
+	fmt.Println("Name: ", pers2.name)
+	fmt.Println("Age: ", pers2.age)
+	fmt.Println("Job: ", pers2.job)
+	fmt.Println("Salary: ", pers2.salary)
 }
