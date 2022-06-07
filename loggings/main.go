b9d4e8a95bebf88ef043bf5826cd4325c5f7cf74
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-06-07 18:55:58 -0300

    Log em arquivo de texto

--- loggings/main.go
+++ loggings/main.go
@@ -1,5 +1,11 @@
 package main
-import "log"
+import (
+	"log"
+	"os"
+)
 func main() {
+	logFile, _ := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	defer logFile.Close()
+	log.SetOutput(logFile)
 	log.Println("Hello, world!")
 }
