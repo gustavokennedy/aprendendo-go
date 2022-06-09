27f0f0e694b6a0bd0c542c9b5cd7a62ba243117b
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-06-09 16:10:52 -0300

    Fazendo Consultas

--- graphql-go-howto/graph/schema.resolvers.go
+++ graphql-go-howto/graph/schema.resolvers.go
@@ -27,7 +27,14 @@ func (r *mutationResolver) RefreshToken(ctx context.Context, input model.Refresh
 }
 
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
-	panic(fmt.Errorf("not implemented"))
+  var links []*model.Link
+  dummyLink := model.Link{
+    Title: "our dummy link",
+    Address: "https://address.org",
+    User: &model.User{Name: "admin"},
+  }
+	links = append(links, &dummyLink)
+	return links, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -38,16 +45,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
